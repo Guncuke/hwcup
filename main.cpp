e80c94d93e2b5db44ac0216bbbcc37aa47da34ee
volcano <78009909+Guncuke@users.noreply.github.com>
2024-03-22 15:30:13 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,5 @@
 #include <bits/stdc++.h>
-#include<fstream>
 using namespace std;
-// fstream f1;
 
 const int n = 200;
 const int robot_num = 10;
@@ -12,7 +10,7 @@ const int dxs[] = {0, 0, -1, 1};
 const int dys[] = {1, -1, 0, 0};
 // 泊位效率，金钱，和时间的权重
 const float quanzhong_efficiency = 10;
-const float quanzhong_money = 1;
+const float quanzhong_money = 20;
 const float quanzhong_time = 1;
 // 金钱，船只容量，当前帧数
 int money, boat_capacity, id;
@@ -81,8 +79,8 @@ struct Berth
     deque<int> items;
     // 总价值
     int total_value;
-    // 是否被占用(有船在装卸和有船前往都算被占用)
-    bool is_occupied;
+    // 最小剩余价值
+    int min_value;
     Berth(){}
     Berth(int x, int y, int transport_time, int loading_speed) {
         this -> x = x;
@@ -91,9 +89,6 @@ struct Berth
         this -> loading_speed = loading_speed;
     }
 }berth[berth_num + 10];
-
-// 泊位优先级
-vector<Berth> berth_priority(berth_num);
 
 struct Boat
 {   
@@ -327,14 +322,47 @@ int GetDirection(pair<int, int> a, pair<int, int> b) {
     }
 }
 
+// 获得当前每个泊位的最小价值
+// 即每个泊位扣去被目前的船装完剩下的价值
+void GetMinValue(){
+    int berth_residue_item[10] = {0};
+    for(int i = 0; i < boat_num; i++){
+        if(boat[i].pos==-1) continue;
+        berth_residue_item[boat[i].pos] += boat_capacity - boat[i].num;
+    }
+    // 每个泊位当前剩余价值
+    for(int i = 0; i < berth_num; i++){
+        berth[i].min_value = berth[i].total_value;
+        // 没有船来，价值就是泊位价值
+        if(berth_residue_item[i] == 0) continue;
+        // 如果有船以泊位i为目标，总价值扣除对应数量的物品价值
+        auto it = berth[i].items.begin();
+        for (int b=0; b < berth_residue_item[i] && it != berth[i].items.end(); ++b, ++it){
+            berth[i].min_value -= *it;
+        }
+    }
+}
+
+// 泊位选择
+int ChooseBerth(){
+    // 根据泊位剩余价值/效率/时间 选择最大的一个
+    int max_index = 0;
+    float max_value = berth[0].min_value*quanzhong_money + berth[0].loading_speed*quanzhong_efficiency - berth[0].transport_time*quanzhong_time;
+    for(int i = 1; i < berth_num; i++){
+        if(berth[i].min_value*quanzhong_money + berth[i].loading_speed*quanzhong_efficiency - berth[i].transport_time*quanzhong_time > max_value){
+            max_index = i;
+            max_value = berth[i].min_value*quanzhong_money + berth[i].loading_speed*quanzhong_efficiency - berth[i].transport_time*quanzhong_time;
+        }
+    }
+    return max_index;
+}
+
 int main()
 {   
-    // f1.open("data.txt",ios::out);
     Init();
     for(int zhen = 1; zhen <= 15000; zhen ++)
     {
         int id = Input();
-        // f1 << "id: " << id << endl;
         // 机器人部分
         // 首先要判断服务器是否正确移动，如果没有正确移动，回到寻路状态
         // 然后判断机器人是否处于正常状态，如果处于恢复状态去状态6
@@ -353,8 +381,8 @@ int main()
                     bot.zt--;
                     continue;
                 }
-                // 每20帧重新计算一个机器人的新路径
-                if(i == zhen % 20 && zhen > 20 && bot.zt == 1) {
+                // 每10帧重新计算一个机器人的新路径
+                if(i == (zhen-1) % 10 && zhen > 10 && bot.zt == 1) {
                     bot.zt = 0;
                 }
                 // 正常移动
@@ -362,22 +390,24 @@ int main()
             }
         }
         // 之后，所有处于寻路状态的机器人位置都正确，位于current_index上
+        int bfs_time = 0;
         for(int bot_num = 0; bot_num < robot_num; bot_num++){
             Robot& bot = robot[bot_num];
             // 机器人包括6个状态
             // 0:寻找去货物的最短路 1:运输途中 2: 到达货物点 3: 寻找到泊位的最短路 4：去往泊位 5: 到达泊位 6：恢复状态
-            // f1 << "bot_num: " << bot_num << " status: " << bot.zt << endl;
             switch (bot.zt)
             {
             // 状态0：寻路物品
             case 0:{
                 find_item:
                 bot.clearPath();
-                if(bot.bfs_fail_time > 20) {
+                if(bot.bfs_fail_time >=50) {
                     break;
                 }
+                if(bfs_time>=1) break; 
                 vector<vector<pair<int, int>>> paths = BFSItem({bot.x, bot.y}, bot_num);
                 vector<pair<int, int>> path = ChoosePath(paths);
+                bfs_time++;
                 if(path.empty()) {
                     bot.bfs_fail_time++;
                     break;
@@ -501,39 +531,38 @@ int main()
 
         }
 
-        // 0: 移动 1: 正常 2:等待
-        // 出发选择
-        // 将berth_priority复制一份，用于排序
-        for(int i = 0; i < berth_num; i++){
-            berth_priority[i] = berth[i];
-        }
-        // 根据泊位价值/效率/时间排序
-        sort(berth_priority.begin(), berth_priority.end(), [&](const Berth& a, const Berth& b) {
-            return a.total_value * quanzhong_money + a.loading_speed * quanzhong_efficiency - a.transport_time * quanzhong_time > b.total_value * quanzhong_money + b.loading_speed * quanzhong_efficiency - b.transport_time * quanzhong_time;
-        });
-
-        // 每个泊位的船只数量（包括在路上和在泊位）
-        int boat_num_in_berth[10] = {0};
+        // 船只部分
+        // 首先获得每个泊位当前的最小价值，存在berth[i].min_value中
+        GetMinValue();
+        // 在虚拟点
         for(int i = 0; i < boat_num; i++){
-            if(boat[i].pos==-1) continue;
-            boat_num_in_berth[boat[i].pos]++;
-        }
+            // 第一帧单独判断一下，因为没有货物，直接去时间最短的泊位
+            if(id == 1){
+                vector<Berth> temp(10);
+                for(int j = 0; j < 10; j++){
+                    temp[j] = berth[j];
+                }
+                sort(temp.begin(), temp.end(), [](const Berth& a, const Berth& b){
+                    return a.transport_time < b.transport_time || (a.transport_time == b.transport_time && a.loading_speed > b.loading_speed);
+                });
 
-        for(int i = 0; i < boat_num; i++){
-            // TODO: 如果在虚拟点，通过1.泊位价值+2.路径花费时间+3.装载速度 决定去哪个泊位
+                printf("ship %d %d\n", i, temp[i].id);
+                continue;
+            }
+            // 如果在虚拟点，出发去泊位
             if(boat[i].status == 1 && boat[i].pos== -1) {
                 boat[i].num = 0;
-                for(int j = 0; j < berth_num; j++){
-                    if(berth[berth_priority[j].id].items.size()/boat_capacity >= boat_num_in_berth[berth_priority[j].id]){
-                        boat_num_in_berth[berth_priority[j].id]++;
-                        printf("ship %d %d\n", i, berth_priority[j].id);
-                        break;
-                    }
-                }
+                int target_berth = ChooseBerth();
+                boat[i].pos = target_berth;
+                // 重新计算最小价值，target_berth的min_value在当前帧已经被扣除
+                GetMinValue();
+                // 防止进入下面的循环
+                boat[i].status = 0;
+                printf("ship %d %d\n", i, target_berth);
             }
         }
 
-        // 泊位装货，维持泊位信息与出发
+        // 在泊位
         for(int i = 0; i < boat_num; i ++)
         {   
             // 在泊位
@@ -545,35 +574,31 @@ int main()
                     printf("go %d\n", i);
                     break;
                 }
-                // 泊位可以给那么多
+                // 装货
                 int upload_num = min(berth[target_berth].loading_speed, (int)berth[target_berth].items.size());
-                // 船上还能装多少
                 upload_num = min(upload_num, boat_capacity - boat[i].num);
                 boat[i].num += upload_num;
                 for(int j = 0; j < upload_num; j ++){
                     berth[target_berth].total_value -= berth[target_berth].items.front();
                     berth[target_berth].items.pop_front();
-                }                    
+                }
+
                 // 货满了出发
                 if(boat[i].num == boat_capacity) {
                     printf("go %d\n", i);
-                    boat_num_in_berth[target_berth]--;
                     break;
                 }
+
                 // 没货了，船未满一半，且时间充裕，去别的泊位
                 if(berth[target_berth].items.size() == 0) {
-                    if(boat[i].num < boat_capacity*3/4 && berth[target_berth].transport_time < 14490 - id) {
-                        for(int j = 0; j < berth_num; j++){
-                            if(berth[berth_priority[j].id].items.size()/boat_capacity >= boat_num_in_berth[berth_priority[j].id]){
-                                printf("ship %d %d\n", i, berth_priority[j].id);
-                                boat_num_in_berth[berth_priority[j].id]++;
-                                break;
-                            }
-                        }
+                    if(boat[i].num < boat_capacity*1/2 && berth[target_berth].transport_time < 14490 - id) {
+                        int target_berth = ChooseBerth();
+                        boat[i].pos = target_berth;
+                        GetMinValue();
+                        printf("ship %d %d\n", i, target_berth);
                     }
                     else{
                         printf("go %d\n", i);
-                        boat_num_in_berth[target_berth]--;
                         break;
                     }
                     break;
@@ -583,6 +608,5 @@ int main()
         puts("OK");
         fflush(stdout);
     }
-    // f1.close();
     return 0;
 }
